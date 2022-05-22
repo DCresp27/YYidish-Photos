427bf880228a5f4378fe4cc64716cc8253682d50
DanCrespin <danielc727@gmail.com>
2022-05-22 16:20:22 -0400

    Updated About Page
    Finished Ajax on about page.

--- js/about.js
+++ js/about.js
@@ -1,6 +1,19 @@
 
 $(document).ready(function(){
-    $(".btn").click(function(){
-        $("#box").load("/Users/dannycrespin/WebstormProjects/YYDish-Photos/html/ajaxAbout.html");
+    $("#submit").click(function(){
+        var isValid = true;
+        var email = $("#email").val();
+        if(email === "" || !/^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/.test(email)) {
+            isValid = false;
+            $("#signup").next().text("Please enter a valid email.");
+        }
+        if (isValid) {
+            $("#message").hide();
+            $("#signup").hide();
+            $("#box").load("../html/ajaxAbout.html");
+        }
+
+
+
     });
 });
